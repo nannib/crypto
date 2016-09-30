973780f6b53daa4c8ec0d23ff4639b238da2f0a2
Nanni Bassetti <nannib@users.noreply.github.com>
2016-09-30 19:31:24 +0200

    Add files via upload

---
+++ rsa_crack.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python2
+# RSA_CRACK.py
+# Cracking RSA key - finding the two prime numbers those make the key
+# finding the private exponent
+# this is only for didactic purposes.
+# by Nanni Bassetti - http://www.nannibassetti.com - digitfor@gmail.com
+
+import random, math
+
+# n is the key
+n = 824296325825751113
+# e is the public exponent
+e = 65537
+q=int(math.sqrt(n))
+
+# factorize
+r1 = 1
+while r1 != 0 :
+	q = q+1
+#	print "q=",q
+	r1 = n % q
+	p = n/q
+
+print 'p =',p,'q =',q
+# finding d (private exponent)
+k=0
+f=(p-1)*(q-1)
+r2=1
+while r2 != 0 :
+	k=random.randint(1,e)
+	r2=(f*k+1) % e
+#print 'k=',k
+d=(f*k+1) / e
+print 'd=',d 
+
